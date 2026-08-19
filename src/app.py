837f69b729868f46900ba595aee0c01b60d3f98e
ChrisF <chrisferbia@gmail.com>
2026-08-19 23:50:12 +0700

    Enable CORS

--- src/app.py
+++ src/app.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import api
 import ui
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://money-manager-fe.azamines.workers.dev",
+        "http://localhost:5173",
+    ],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(api.router)
 app.include_router(ui.router)
